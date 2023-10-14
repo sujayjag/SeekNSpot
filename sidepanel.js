08ff371402b4db7258fcdc9f7b624b2793f69659
Sanjay Mohandas <mohandasnj@gmail.com>
2023-10-14 15:26:36 -0400

    fwevr
    erv

--- sidepanel.js
+++ sidepanel.js
@@ -1,61 +1,63 @@
-// chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-//         let currentTab = tabs[0];
-//         let urlElem = document.getElementById('url');
-//         let iframeElem = document.getElementById('streamlitFrame');
-    
-//         if (currentTab && currentTab.url && currentTab.url.includes('youtube.com')) {
-//             urlElem.textContent = currentTab.url;
-//         } else {
-//             urlElem.textContent = "Not a YouTube page!";
-//         }
-//         //console.log(urlElem);
-//     });
-
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     let currentTab = tabs[0];
-    let urlElem = document.getElementById('url');
-    let iframeElem = document.getElementById('streamlitFrame');
-    
+
     if (currentTab && currentTab.url && currentTab.url.includes('youtube.com')) {
-        urlElem.textContent = currentTab.url;
-        let newIframeSrc = iframeElem.src + currentTab.url;
-        iframeElem.src = newIframeSrc;
+        const youtubeURL = currentTab.url;
+
+        // Send the YouTube URL to your Flask backend
+        fetch('http://localhost:5001/receive-youtube-url', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ youtubeURL }),
+        }).then(() => console.log("POST REQUEST SUCCESSFUL")).then(response => console.log(JSON.stringify(response)));
+       
+
+        // Optionally, store the URL in a variable for later use
+        // const storedURL = youtubeURL;
     } else {
-        urlElem.textContent = "Not a YouTube page!";
+        // Handle the case when the current page is not a YouTube page
+        alert("You're not on a YouTube page! Please retry the extension with a Youtube page!");
+        setTimeout(function() {
+            window.close();
+        }, 1000);
     }
 });
 
-// function checkForNewURL() {
-//     let desiredURL = document.querySelector('meta[name="desired-url"]').getAttribute('content');
-
-//     if (desiredURL) {
-//         chrome.tabs.update({ url: desiredURL });
-//     }
-// }
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     let iframeContent = document.getElementById("streamlitFrame").contentWindow;
+document.addEventListener('DOMContentLoaded', function() {
+    const openUrlButton = document.getElementById('openUrlButton');
     
-//     iframeContent.document.querySelector("button:contains('link change test')").addEventListener('click', function() {
-//         checkForNewURL();
-//     });
-// });
+    openUrlButton.addEventListener('click', function() {
+        // Fetch the URL from the backend
+        fetch('http://localhost:5001/get-url-file')
+            .then(response => response.json())
+            .then(data => {
+                const url = data.url;
+                console.log("Received URL:", url);
+                
+                // Update the current tab with the received URL
+                chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                    const currentTab = tabs[0];
+                    if (currentTab && url) {
+                        chrome.tabs.update(currentTab.id, { url });
+                        //chrome.runtime.reload();
+                        //window.location.reload();
+                        //chrome.tabs.reload(tabs.id);
+                    } else {
+                        alert("URL is empty or doesn't exist");
+                    }
+                });
+            })
+            .catch(error => {
+                console.error("Error fetching URL:", error);
+                alert("Failed to fetch URL");
+            });
+    });
+});
 
-// let iframeContent = document.getElementById("streamlitFrame").contentWindow;
-// console.log(iframeContent);
-// let buttons = iframeContent.document.querySelectorAll("button");
-// let targetButton;
-
-// for (let btn of buttons) {
-//     console.log(btn.textContent);
-//     if (btn.textContent.includes('link change test')) {
-//         targetButton = btn;
-//         break;
-//     }
-// }
-
-// if (targetButton) {
-//     targetButton.addEventListener('click', function() {
-//         checkForNewURL();
-//     });
-// }
+document.addEventListener('click', function(event) {
+    if (event.target.tagName === 'A') {
+        window.location.reload();
+    }
+});
